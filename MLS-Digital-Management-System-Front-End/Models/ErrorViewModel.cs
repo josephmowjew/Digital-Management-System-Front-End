e291c925de459208a752a42aa0e84f7a43e3a4fc
Joseph Mojoo <61784861+Joseph-Mojoo@users.noreply.github.com>
2024-04-22 09:30:37 +0200

    initial commit

---
+++ MLS-Digital-Management-System-Front-End/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MLS_Digital_Management_System_Front_End.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

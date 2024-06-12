ec6a263063dc3737e29c7f4fb4202ae469674fce
ArthurKawalewale-SparcSys <arthur.kawalewale@sparcsystems.africa>
2024-06-12 10:33:45 +0200

    finalized work on financial controller's section implementing penalty payments

---
+++ MLS-Digital-Management-System-Front-End/Areas/Finance/Controllers/PenaltiesController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MLS_Digital_Management_System_Front_End.Core.DTOs.Member;
+using MLS_Digital_Management_System_Front_End.Core.DTOs.YearOfOperation;
+using MLS_Digital_Management_System_Front_End.Helpers;
+using MLS_Digital_Management_System_Front_End.Services.Interfaces;
+
+namespace MLS_Digital_Management_System_Front_End.Areas.Finance.Controllers
+{
+    [Area("Finance")]
+    public class PenaltiesController : Controller
+    {
+        private readonly IServiceRepository _service;
+
+        public PenaltiesController(IServiceRepository serviceRepository)
+        {
+            _service = serviceRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            await PopulateViewBags();
+
+            return View();
+
+
+        }
+
+        private async Task PopulateViewBags()
+        {
+            //get the token
+            string token = AuthHelper.GetToken(HttpContext);
+            ViewBag.token = token;
+            _service.Token = token;
+            //ViewBag.memberId = await GetMemberByUserIdAsync();
+            ViewBag.YearOfOperation = await GetCurrentYearOfOperationAsync();
+            //ViewBag.membersList = await GetAllMembersAsync();
+            ViewBag.penaltyList = await GetAllPenaltiesAsync();
+            //ViewBag.penaltyTypeList = await GetAllPenaltyTypesAsync();
+            ViewBag.yearOperationsList = await GetYearOfOperations();
+        }
+
+        private async Task<ReadYearOfOperationDTO> GetCurrentYearOfOperationAsync()
+        {
+            return await _service.YearOfOperationService.GetCurrentYearOfOperationAsync();
+        }
+
+        /*private async Task<List<SelectListItem>> GetAllMembersAsync()
+        {
+            List<SelectListItem> membersList = new() { new SelectListItem() { Text = "---Select Member---", Value = "" } };
+
+            //get identity types from remote
+            var memberListFromRemote = await _service.MemberService.GetAllMembersAsync();
+
+            if (memberListFromRemote != null)
+            {
+                memberListFromRemote.ForEach(member =>
+                {
+                    membersList.Add(new SelectListItem() { Text = member.User.FullName + " - " + member.User.IdentityNumber, Value = member.Id.ToString() });
+                });
+            }
+
+            return membersList;
+        }*/
+
+        /*private async Task<List<SelectListItem>> GetAllPenaltyTypesAsync()
+        {
+            List<SelectListItem> penaltyTypesList = new() { new SelectListItem() { Text = "---Select Penalty Types---", Value = "" } };
+
+            //get identity types from remote
+            var penaltyTypesListFromRemote = await _service.PenaltyTypeService.GetAllPenaltyTypesAsync();
+
+            if (penaltyTypesListFromRemote != null)
+            {
+                penaltyTypesListFromRemote.ForEach(penaltyType =>
+                {
+                    penaltyTypesList.Add(new SelectListItem() { Text = penaltyType.Name, Value = penaltyType.Id.ToString() });
+                });
+            }
+
+            return penaltyTypesList;
+        }*/
+
+        private async Task<List<SelectListItem>> GetYearOfOperations()
+        {
+            List<SelectListItem> yearOperationsList = new() { new SelectListItem() { Text = "---Select Practicing Year---", Value = "" } };
+
+            //get identity types from remote
+            var yearsOfOperationFromRemote = await _service.YearOfOperationService.GetAllYearOfOperationsAsync();
+
+            if (yearsOfOperationFromRemote != null)
+            {
+                yearsOfOperationFromRemote.ForEach(year =>
+                {
+                    yearOperationsList.Add(new SelectListItem() { Text = year.StartDate.ToString("yyyy") + " - " + year.EndDate.ToString("yyyy"), Value = year.Id.ToString() });
+                });
+            }
+
+            return yearOperationsList;
+        }
+
+        private async Task<List<SelectListItem>> GetAllPenaltiesAsync()
+        {
+            List<SelectListItem> penaltiesList = new() { new SelectListItem() { Text = "---Select Penalty---", Value = "" } };
+
+            //get identity types from remote
+            var penaltiesListFromRemote = await _service.PenaltyService.GetAllPenaltiesAsync();
+
+            if (penaltiesListFromRemote != null)
+            {
+                penaltiesListFromRemote.ForEach(penalty =>
+                {
+                    penaltiesList.Add(new SelectListItem() { Text = penalty.Reason, Value = penalty.Id.ToString() });
+                });
+            }
+
+            return penaltiesList;
+        }
+    }
+}

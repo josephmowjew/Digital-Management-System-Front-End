c221274d47a517d838974af684b331219f429847
ArthurKawalewale-SparcSys <arthur.kawalewale@sparcsystems.africa>
2024-07-02 09:55:20 +0200

    fixed view processed invoices

--- MLS-Digital-Management-System-Front-End/Services/Interfaces/IInvoiceRequestService.cs
+++ MLS-Digital-Management-System-Front-End/Services/Interfaces/IInvoiceRequestService.cs
@@ -1,8 +1,11 @@
 using MLS_Digital_Management_System_Front_End.Core.DTOs.InvoiceRequest;
+using MLS_Digital_Management_System_Front_End.Core.DTOs.QBInvoices;
 
 namespace MLS_Digital_Management_System_Front_End.Services.Interfaces;
 
 public interface IInvoiceRequestService
 {
     Task<ReadInvoiceRequestDTO> GetInvoiceRequestByIdAsync(int id);
+
+    Task<ReadQBInvoiceDTO> GetProcessedInvoiceRequestByIdAsync(string id);
 }
--- MLS-Digital-Management-System-Front-End/Services/InvoiceRequestService.cs
+++ MLS-Digital-Management-System-Front-End/Services/InvoiceRequestService.cs
@@ -1,4 +1,5 @@
 using MLS_Digital_Management_System_Front_End.Core.DTOs.InvoiceRequest;
+using MLS_Digital_Management_System_Front_End.Core.DTOs.QBInvoices;
 using MLS_Digital_Management_System_Front_End.Services.Interfaces;
 
 namespace MLS_Digital_Management_System_Front_End.Services;
@@ -17,6 +18,20 @@ public class InvoiceRequestService : IInvoiceRequestService
         if (response.IsSuccessStatusCode)
         {
             var responseData = await response.Content.ReadFromJsonAsync<ReadInvoiceRequestDTO>();
+            return responseData;
+        }
+        else
+        {
+            throw new InvalidOperationException($"Failed to get invoice request with id {id}.");
+        }
+    }
+
+    public async Task<ReadQBInvoiceDTO> GetProcessedInvoiceRequestByIdAsync(string id)
+    {
+        var response = await _httpClient.GetAsync($"api/invoiceRequest/processed/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            var responseData = await response.Content.ReadFromJsonAsync<ReadQBInvoiceDTO>();
             return responseData;
         }
         else

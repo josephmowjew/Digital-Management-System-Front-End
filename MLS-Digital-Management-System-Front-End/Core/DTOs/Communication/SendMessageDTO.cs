f1595c74d023ce5d3dbb5d234c5209ebe6c125bb
Joseph Mojoo <61784861+Joseph-Mojoo@users.noreply.github.com>
2024-07-18 21:46:14 +0200

    added UI to send announcements to secretariat

---
+++ MLS-Digital-Management-System-Front-End/Core/DTOs/Communication/SendMessageDTO.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MLS_Digital_Management_System_Front_End.Core.DTOs.Communication
+{
+    public class SendMessageDTO
+    {
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        public List<int> DepartmentIds { get; set; } = new List<int>();
+
+        public List<string> RoleNames { get; set; } = new List<string>();
+
+        public bool SendToAllUsers { get; set; }
+    }
+}

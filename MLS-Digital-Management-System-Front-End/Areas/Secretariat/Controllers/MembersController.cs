0e0e3bda3fab89c6c7074f48665b0fca825d2711
ArthurKawalewale-SparcSys <arthur.kawalewale@sparcsystems.africa>
2024-09-03 08:54:02 +0200

    added members section in secretariat page

---
+++ MLS-Digital-Management-System-Front-End/Areas/Secretariat/Controllers/MembersController.cs
@@ -0,0 +1,51 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MLS_Digital_Management_System_Front_End.Helpers;
+using MLS_Digital_Management_System_Front_End.Services.Interfaces;
+
+namespace MLS_Digital_Management_System_Front_End.Areas.Secretariat.Controllers
+{
+    [Area("Secretariat")]
+    public class MembersController : Controller
+    {
+        private readonly IServiceRepository _service;
+
+        public MembersController(IServiceRepository serviceRepository)
+        {
+            _service = serviceRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            await PopulateViewBags();
+            return View();
+        }
+
+        public async Task<IActionResult> LicensedMembers()
+        {
+            await PopulateViewBags();
+            return View();
+        }
+
+        public async Task<IActionResult> UnlicensedMembers()
+        {
+            await PopulateViewBags();
+            return View();
+        }
+
+        private async Task PopulateViewBags()
+        {
+            await PopulateViewBagsMinimal();
+        }
+
+        
+
+        private async Task PopulateViewBagsMinimal()
+        {
+            string token = AuthHelper.GetToken(HttpContext);
+            ViewBag.token = token;
+            _service.Token = token;
+        }
+    }
+}

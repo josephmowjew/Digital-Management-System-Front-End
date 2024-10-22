847af261ca1f35d9995b9cea389375c7190c0e91
Joseph Mojoo <joseph@sparcsystems.africa>
2024-10-22 16:21:55 +0200

    added messaging and stamps section to ceo

---
+++ MLS-Digital-Management-System-Front-End/Areas/CEO/Controllers/CommunicationsController.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MLS_Digital_Management_System_Front_End.Core.DTOs.YearOfOperation;
+using MLS_Digital_Management_System_Front_End.Helpers;
+using MLS_Digital_Management_System_Front_End.Services.Interfaces;
+
+namespace MLS_Digital_Management_System_Front_End.Areas.CEO.Controllers
+{
+    [Area("CEO")]
+    public class CommunicationsController : Controller
+    {
+        private readonly IServiceRepository _service;
+
+        public CommunicationsController(IServiceRepository serviceRepository)
+        {
+            _service = serviceRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            await PopulateViewBags();
+            return View();
+        }
+
+        private async Task PopulateViewBags()
+        {
+            string token = AuthHelper.GetToken(HttpContext);
+            ViewBag.token = token;
+            _service.Token = token;
+           
+
+            // Populate departments and roles for the dropdowns
+            ViewBag.Departments = await this.GetDepartments();
+            ViewBag.Roles = await this.GetRoles();
+        }
+
+     
+
+        private async Task<List<SelectListItem>> GetRoles()
+        {
+            List<SelectListItem> rolesList = new() { new SelectListItem() { Text = "---Select Role---", Value = "" } };
+            //get identity types from remote
+            var rolesFromRemote =  await this._service.RoleService.GetAllRolesAsync();
+
+            if (rolesFromRemote != null)
+            {
+                rolesFromRemote.ForEach(role =>
+                {
+                    rolesList.Add(new SelectListItem() { Text = role.Name, Value = role.Name.ToString() });
+                });
+            }
+            return rolesList;
+        }
+
+        private async Task<List<SelectListItem>> GetDepartments()
+        {
+            List<SelectListItem> departmentsList = new() { new SelectListItem() { Text = "---Select Departments---", Value = "" } };
+            //get identity types from remote
+            var departmentsFromRemote =  await this._service.DepartmentService.GetAllDepartmentsAsync();
+
+            if (departmentsFromRemote != null)
+            {
+                departmentsFromRemote.ForEach(department =>
+                {
+                    departmentsList.Add(new SelectListItem() { Text = department.Name, Value = department.Id.ToString() });
+                });
+            }
+            return departmentsList;
+        }
+    }
+}
---
+++ MLS-Digital-Management-System-Front-End/Areas/CEO/Controllers/StampsController.cs
@@ -0,0 +1,70 @@
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MLS_Digital_Management_System_Front_End.Core.DTOs.YearOfOperation;
+using MLS_Digital_Management_System_Front_End.Helpers;
+using MLS_Digital_Management_System_Front_End.Services.Interfaces;
+
+namespace MLS_Digital_Management_System_Front_End.Areas.CEO.Controllers
+{
+    [Authorize]
+    [Area("CEO")]
+    public class StampsController : Controller
+    {
+        private readonly IServiceRepository _service;
+
+        public StampsController(IServiceRepository serviceRepository)
+        {
+            _service = serviceRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            await PopulateViewBags();
+            return View();
+        }
+
+        private async Task PopulateViewBags()
+        {
+            string token = AuthHelper.GetToken(HttpContext);
+            ViewBag.token = token;
+            this._service.Token = token;
+            ViewBag.yearOperationsList = await GetYearOfOperations();
+        }
+
+        private async Task<List<SelectListItem>> GetYearOfOperations()
+        {
+            List<SelectListItem> yearOperationsList = new() { new SelectListItem() { Text = "---Select Practicing Year---", Value = "" } };
+
+            //get identity types from remote
+            var yearsOfOperationFromRemote = await this._service.YearOfOperationService.GetAllYearOfOperationsAsync();
+
+            if (yearsOfOperationFromRemote != null)
+            {
+                yearsOfOperationFromRemote.ForEach(year =>
+                {
+                    yearOperationsList.Add(new SelectListItem() { Text = year.StartDate.ToString("yyyy") + " - " + year.EndDate.ToString("yyyy"), Value = year.Id.ToString() });
+                });
+            }
+
+            return yearOperationsList;
+        }
+
+        public async Task<IActionResult> StampDetails(int Id)
+        {
+
+            await PopulateViewBags();
+            var stamp = await _service.StampService.GetStampByIdAsync(Id);
+
+            if (stamp == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.stamp = stamp;
+
+            return View();
+        }
+    }
+}

5994d5f8ebafff719bbc5c6de95ddc07e456a0c6
ArthurKawalewale-SparcSys <arthur.kawalewale@sparcsystems.africa>
2024-10-25 13:00:15 +0200

    work on the notaries listing and allowing the secretariat to create notaries public

--- MLS-Digital-Management-System-Front-End/Areas/Secretariat/Controllers/NotariesPublicController.cs
+++ MLS-Digital-Management-System-Front-End/Areas/Secretariat/Controllers/NotariesPublicController.cs
@@ -45,13 +45,54 @@ namespace MLS_Digital_Management_System_Front_End.Areas.Secretariat.Controllers
             await PopulateViewBagsMinimal();
         }
 
-        
+
 
         private async Task PopulateViewBagsMinimal()
         {
             string token = AuthHelper.GetToken(HttpContext);
             ViewBag.token = token;
             _service.Token = token;
+            ViewBag.membersList = await GetAllMembersAsync();
+            ViewBag.yearOfOperationsList = await GetYearOfOperations();
+        }
+
+        private async Task<List<SelectListItem>> GetAllMembersAsync()
+        {
+            List<SelectListItem> membersList = new() { new SelectListItem() { Text = "---Select Member---", Value = "" } };
+
+            //get identity types from remote
+            var memberListFromRemote = await this._service.MemberService.GetAllMembersAsync();
+
+            if (memberListFromRemote != null)
+            {
+                memberListFromRemote.ForEach(member =>
+                {
+                    if (member.CustomerId != null)
+                    {
+                        membersList.Add(new SelectListItem() { Text = member.User.FullName + " - " + member.User.IdentityNumber, Value = member.Id.ToString() });
+                    }
+                });
+            }
+
+            return membersList;
+        }
+
+        private async Task<List<SelectListItem>> GetYearOfOperations()
+        {
+            List<SelectListItem> yearOperationsList = new() { new SelectListItem() { Text = "---Select Practicing Year---", Value = "" } };
+
+            //get identity types from remote
+            var yearsOfOperationFromRemote = await _service.YearOfOperationService.GetAllYearOfOperationsAsync();
+
+            if (yearsOfOperationFromRemote != null)
+            {
+                yearsOfOperationFromRemote.ForEach(year =>
+                {
+                    yearOperationsList.Add(new SelectListItem() { Text = year.StartDate.ToString("yyyy") + " - " + year.EndDate.ToString("yyyy"), Value = year.Id.ToString() });
+                });
+            }
+
+            return yearOperationsList;
         }
     }
 }

bbe74cce5fc6660b9ff6aa744ead3d2c0c9b90e6
ArthurKawalewale-SparcSys <arthur.kawalewale@sparcsystems.africa>
2024-10-31 10:14:54 +0200

    work on allowing ceo to update member profile details

--- MLS-Digital-Management-System-Front-End/Areas/CEO/Controllers/MembersController.cs
+++ MLS-Digital-Management-System-Front-End/Areas/CEO/Controllers/MembersController.cs
@@ -39,13 +39,64 @@ namespace MLS_Digital_Management_System_Front_End.Areas.CEO.Controllers
             await PopulateViewBagsMinimal();
         }
 
-        
-
         private async Task PopulateViewBagsMinimal()
         {
             string token = AuthHelper.GetToken(HttpContext);
             ViewBag.token = token;
             _service.Token = token;
+            ViewBag.identityTypesList = await GetIdentityTypes();
+            ViewBag.personalTitlesList = await GetPersonalTitles();
+            ViewBag.countriesList = await GetCountries();
+        }
+
+        private async Task<List<SelectListItem>> GetIdentityTypes()
+        {
+            List<SelectListItem> identityTypes = new() { new SelectListItem() { Text = "---Select Identity Type---", Value = "" } };
+
+            //get identity types from remote
+            var identityTypesFromRemote =  await this._service.IdentityTypeService.GetAllIdentityTypesAsync();
+
+            if (identityTypesFromRemote != null)
+            {
+                identityTypesFromRemote.ForEach(idType =>
+                {
+                    identityTypes.Add(new SelectListItem() { Text = idType.Name, Value = idType.Id.ToString() });
+                });
+            }
+
+            return identityTypes;
+        }
+
+        private async Task<List<SelectListItem>> GetPersonalTitles()
+        {
+            List<SelectListItem> personalTitlesList = new() { new SelectListItem() { Text = "---Select Title---", Value = "" } };
+            //get identity types from remote
+            var titlesFromRemote =  await this._service.TitleService.GetAllTitlesAsync();
+
+            if (titlesFromRemote != null)
+            {
+                titlesFromRemote.ForEach(title =>
+                {
+                    personalTitlesList.Add(new SelectListItem() { Text = title.Name, Value = title.Id.ToString() });
+                });
+            }
+            return personalTitlesList;
+        }
+
+        private async Task<List<SelectListItem>> GetCountries()
+        {
+            List<SelectListItem> countriesList = new() { new SelectListItem() { Text = "---Select Country---", Value = "" } };
+            //get countries  from remote
+            var countriesFromRemote =  await this._service.CountryService.GetCountries();
+
+            if (countriesFromRemote != null)
+            {
+                countriesFromRemote.ForEach(country =>
+                {
+                    countriesList.Add(new SelectListItem() { Text = country.Name, Value = country.Id.ToString() });
+                });
+            }
+            return countriesList;
         }
     }
 }

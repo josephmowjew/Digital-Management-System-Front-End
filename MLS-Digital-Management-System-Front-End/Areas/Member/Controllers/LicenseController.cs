64f8aa3ad970d702b260d50ec733da431b90494c
ArthurKawalewale-SparcSys <arthur.kawalewale@sparcsystems.africa>
2024-12-02 15:09:23 +0200

    implement a license section for viewing licenses

---
+++ MLS-Digital-Management-System-Front-End/Areas/Member/Controllers/LicenseController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.Elfie.Serialization;
+using MLS_Digital_Management_System_Front_End.Core.DTOs.LicenseApprovalHistory;
+using MLS_Digital_Management_System_Front_End.Helpers;
+using MLS_Digital_Management_System_Front_End.Services.Interfaces;
+
+namespace MLS_Digital_Management_System_Front_End.Areas.Member.Controllers
+{
+  
+    [Area("Member")]
+    public class LicenseController : Controller
+    {
+        private readonly IServiceRepository _service;
+        public LicenseController(IServiceRepository serviceRepository)
+        {
+            _service = serviceRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            await PopulateViewBags();
+
+            return View();
+        }
+
+        private async Task PopulateViewBags()
+        {
+            //get the token
+            string token = AuthHelper.GetToken(HttpContext);
+            ViewBag.token = token;
+            this._service.Token = token;
+
+            var userId = AuthHelper.GetUserId(HttpContext);
+            var member = await _service.MemberService.GetMemberByUserIdAsync(userId);
+            ViewBag.membership = member != null;
+            ViewBag.member = member;
+        }
+
+        public async Task<IActionResult> LicenseCertificate(int id)
+        {
+            await PopulateViewBags();
+
+            var license = await _service.LicenseService.GetLicense(id);
+            ViewBag.license = license;
+            return View();
+        }
+    }
+}

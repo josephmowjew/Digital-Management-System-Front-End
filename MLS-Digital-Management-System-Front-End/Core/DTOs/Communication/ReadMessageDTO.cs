5e5fce2dc13e850f85fa4a155ef684165f69f24c
ArthurKawalewale-SparcSys <arthur.kawalewale@sparcsystems.africa>
2025-01-09 00:36:32 +0200

    included TargetedRoles  and TargetedDepartments to ReadMessageDTO

--- MLS-Digital-Management-System-Front-End/Core/DTOs/Communication/ReadMessageDTO.cs
+++ MLS-Digital-Management-System-Front-End/Core/DTOs/Communication/ReadMessageDTO.cs
@@ -8,6 +8,9 @@ namespace MLS_Digital_Management_System_Front_End.Core.DTOs.Communication
     {
         public string Subject { get; set; }
         public string Body { get; set; }
+
+        public List<string> TargetedRoles { get; set; }
+        public List<string> TargetedDepartments { get; set; }
         public List<ReadAttachmentDTO> Attachments { get; set; } = new List<ReadAttachmentDTO>();
     }
 }

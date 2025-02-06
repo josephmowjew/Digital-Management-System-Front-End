b156a195410a6ea84cb8c994247295e3c0731003
ArthurKawalewale-SparcSys <arthur.kawalewale@sparcsystems.africa>
2025-02-06 17:18:54 +0200

    initial works on cpd trainings adjustments

--- MLS-Digital-Management-System-Front-End/Core/DTOs/CPDTraining/CreateCPDTraining.cs
+++ MLS-Digital-Management-System-Front-End/Core/DTOs/CPDTraining/CreateCPDTraining.cs
@@ -3,47 +3,55 @@ using System.ComponentModel.DataAnnotations;
 namespace MLS_Digital_Management_System_Front_End.Core.DTOs.CPDTraining;
 
 public class CreateCPDTrainingDTO
-    {
-        [Required]
-        [StringLength(maximumLength: 250)]
-        public string Title { get; set; }
+{
+    [Required]
+    [StringLength(maximumLength: 250)]
+    public string Title { get; set; }
 
-        [StringLength(maximumLength: 250)]
-        public string Description { get; set; }
+    [StringLength(maximumLength: 250)]
+    public string Description { get; set; }
 
-        [Required]
-        [Display(Name = "Duration (in hours)")]
-        public double Duration { get; set; }
+    [Required]
+    [Display(Name = "Duration (in hours)")]
+    public double Duration { get; set; }
 
-        [Required]
-        [Display(Name = "Date To Be Conducted")]
-        public DateTime DateToBeConducted { get; set; }
-        [Display(Name = "Registration Due Date")]
-        [DataType(DataType.Date)]
-        public DateTime? RegistrationDueDate { get; set; }
-        [Required]
-        [Display(Name = "Proposed Units")]
-        public int ProposedUnits { get; set; }
-        [Display(Name="Member Physical Attendance Fee (MWK)")]
-        public double? MemberPhysicalAttendanceFee { get; set; } 
-        [Display(Name = "Member Virtual Attendance Fee (MWK)")]
-        public double? MemberVirtualAttendanceFee { get; set; } 
-        [Display(Name = "Non-Member Physical Attendance Fee (MWK)")]
-        public double? NonMemberPhysicalAttendanceFee { get; set; } 
-        [Display(Name = "Non-Member Virtual Attendance Fee (MWK)")]
-         public double? NonMemberVirtualAttandanceFee { get; set; }
-        [Display(Name = "Physical Venue")]
-        [StringLength(maximumLength:250)]
-        public string? PhysicalVenue { get; set; }
-        [Required]
-        [Display(Name = "CPD Units Awarded")]
-        public int CPDUnitsAwarded { get; set; }
+    [Required]
+    [Display(Name = "Date To Be Conducted")]
+    public DateTime DateToBeConducted { get; set; }
+    [Display(Name = "Registration Due Date")]
+    [DataType(DataType.Date)]
+    public DateTime? RegistrationDueDate { get; set; }
+    [Required]
+    [Display(Name = "Proposed Units")]
+    public int ProposedUnits { get; set; }
+    [Display(Name = "Senior Lawyer Physical Attendance Fee (MWK)")]
+    public double? SeniorLawyerPhysicalAttendanceFee { get; set; }
+    [Display(Name = "Senior Lawyer Virtual Attendance Fee (MWK)")]
+    public double? SeniorLawyerVirtualAttendanceFee { get; set; }
+    [Display(Name = "Junior Lawyer Physical Attendance Fee (MWK)")]
+    public double? JuniorLawyerPhysicalAttendanceFee { get; set; }
+    [Display(Name = "Junior Lawyer Virtual Attendance Fee (MWK)")]
+    public double? JuniorLawyerVirtualAttendanceFee { get; set; }
+    [Display(Name = "Member Physical Attendance Fee (MWK)")]
+    public double? MemberPhysicalAttendanceFee { get; set; }
+    [Display(Name = "Member Virtual Attendance Fee (MWK)")]
+    public double? MemberVirtualAttendanceFee { get; set; }
+    [Display(Name = "Non-Member Physical Attendance Fee (MWK)")]
+    public double? NonMemberPhysicalAttendanceFee { get; set; }
+    [Display(Name = "Non-Member Virtual Attendance Fee (MWK)")]
+    public double? NonMemberVirtualAttandanceFee { get; set; }
+    [Display(Name = "Physical Venue")]
+    [StringLength(maximumLength: 250)]
+    public string? PhysicalVenue { get; set; }
+    [Required]
+    [Display(Name = "CPD Units Awarded")]
+    public int CPDUnitsAwarded { get; set; }
 
-        [StringLength(maximumLength: 200)]
-        [Display(Name = "Accrediting Institution")]
-        public string? AccreditingInstitution { get; set; }
-        [Display(Name = "Poster")]
-        public List<IFormFile?> Attachments = new List<IFormFile>();
+    [StringLength(maximumLength: 200)]
+    [Display(Name = "Accrediting Institution")]
+    public string? AccreditingInstitution { get; set; }
+    [Display(Name = "Poster")]
+    public List<IFormFile?> Attachments = new List<IFormFile>();
 
-        public string? AccreditingInstitutionRepresentativePosition { get; set; }
-    }
+    public string? AccreditingInstitutionRepresentativePosition { get; set; }
+}
